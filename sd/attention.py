6d25aa93fcaeb56eaa502de1fcd31a5000e716bb
jkraft <jasekraft12@gmail.com>
2024-02-14 05:08:40 -0600

    some syntax errors in attention

--- sd/attention.py
+++ sd/attention.py
@@ -4,35 +4,38 @@ from torch.nn import functional as F
 import math
 
 class SelfAttention(nn.Module):
-
-    # d_embed represents the multiple channels of each pixel
-    def __init__(self, n_heads: int, d_embed: int, in_proj_bias=True, out_proj_bias=True):
+    def __init__(self, n_heads, d_embed, in_proj_bias=True, out_proj_bias=True):
         super().__init__()
-        
-        # Multiplying 3 because rgb
-        self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias=in_proj_bias)
+        # This combines the Wq, Wk and Wv matrices into one matrix
+        self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias=in_proj_bias) # Multiplying 3 because rgb
+        # This one represents the Wo matrix
         self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
         self.n_heads = n_heads
         self.d_head = d_embed // n_heads
 
-    def forward(self, x: torch.Tensor, causal_mask=False) -> torch.Tensor:
-        # x: (Batch_Size, Seq_Len, Dim)
-        input_shape = x.shape
-        batch_size, seq_len, d_embed = input_shape
+    def forward(self, x, causal_mask=False):
+        # x: # (Batch_Size, Seq_Len, Dim)
+        print(x.shape)
+        # (Batch_Size, Seq_Len, Dim)
+        input_shape = x.shape 
+        
+        # (Batch_Size, Seq_Len, Dim)
+        batch_size, sequence_length, d_embed = input_shape 
 
-        intermin_shape = (batch_size, seq_len, self.n_heads, self.d_head)
-
+        # (Batch_Size, Seq_Len, H, Dim / H)
+        interim_shape = (batch_size, sequence_length, self.n_heads, self.d_head) 
+        
         # x: (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, 3 * Dim) -> 3 tensors of shape (Batch_Size, Seq_Len, Dim)
         q, k, v = self.in_proj(x).chunk(3, dim=-1)
 
         # (Batch_Size, Seq_len, Dim) -> (Batch_Size, Seq_Len, H, Dim / H) -> (Batch_Size, H, Seq_Len, Dim / H)
-        q = q.view(intermin_shape).transpose(1, 2) # Transpose: https://pytorch.org/docs/stable/generated/torch.transpose.html
-        k = k.view(intermin_shape).transpose(1, 2)
-        v = v.view(intermin_shape).transpose(1, 2)
+        q = q.view(interim_shape).transpose(1, 2) # Transpose: https://pytorch.org/docs/stable/generated/torch.transpose.html
+        k = k.view(interim_shape).transpose(1, 2)
+        v = v.view(interim_shape).transpose(1, 2)
 
         # (Batch_Size, H, Seq_Len, Seq_Len)
         weight = q @ k.transpose(-1, -2) # This is where Queries*Keys are multiplied
-
+    
         # Making the unrelated positions to be -inf so softmax will make them to be 0
         # [Mask Explanation Here](https://youtu.be/bCz4OMemCcA?si=-f5Qxs7dvw_LV2P5)
         if causal_mask: 
@@ -47,7 +50,6 @@ class SelfAttention(nn.Module):
         weight /= math.sqrt(self.d_head)
 
         weight = F.softmax(weight, dim=-1) # Softmax: https://pytorch.org/docs/stable/generated/torch.softmax.html
-
         #  @ = matrix multiplication
         # (Batch_Size, H, Seq_Len, Seq_Len) @ (Batch_Size, H, Seq_Len, Dim / H) -> (Batch_Size, H, Seq_Len, Dim / H)
         output = weight @ v # This is where Queries*Keys and Values are multiplied
@@ -81,16 +83,16 @@ class CrossAttention(nn.Module):
         input_shape = x.shape
         batch_size, seq_len, d_embed = input_shape
 
-        intermin_shape = (batch_size, -1, self.n_heads, self.d_head)
+        interim_shape = (batch_size, -1, self.n_heads, self.d_head)
 
         # Multiplying query by wq ?
         q = self.q_proj(x)
         k = self.k_proj(y)
         v = self.v_proj(y)
 
-        q = q.view(intermin_shape).transpose(1, 2)
-        k = k.view(intermin_shape).transpose(1, 2)
-        v = v.view(intermin_shape).transpose(1, 2)
+        q = q.view(interim_shape).transpose(1, 2)
+        k = k.view(interim_shape).transpose(1, 2)
+        v = v.view(interim_shape).transpose(1, 2)
 
         weight = q @ k.transpose(-1, -2)
 
